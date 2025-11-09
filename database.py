2805ac36ea6e77e8227af7620c8b05c73969610b
SHAMMAH <shamzyjayy@gmail.com>
2025-11-09 13:26:43 +0000

    Fixed foreign key datatype issue and updated Student model

--- database.py
+++ database.py
@@ -1,15 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-
+# Default fallback in case the environment variable is not set
 RENDER_DB_URL = "postgresql://fair_db_user:Qtqs01CtbuJlxU3jQ1b2LyeLxkjeNnQh@dpg-d488aundiees739psf40-a.ohio-postgres.render.com/fair_db"
 
-
-DATABASE_URL = os.environ.get(
-    "postgresql://postgres:guarantee@localhost:5432/fair_db", RENDER_DB_URL
-)
+# Correct: get the DATABASE_URL environment variable if it exists, otherwise use fallback
+DATABASE_URL = os.environ.get("DATABASE_URL", RENDER_DB_URL)
 
 engine = create_engine(DATABASE_URL)
 
