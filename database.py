b544ece7d79deab7dc0265411115e3e0dec8e7f4
SHAMMAH <shamzyjayy@gmail.com>
2025-11-09 13:13:33 +0000

    Fixed foreign key datatype issue and updated Student model

--- database.py
+++ database.py
@@ -7,7 +7,9 @@ import os
 RENDER_DB_URL = "postgresql://fair_db_user:Qtqs01CtbuJlxU3jQ1b2LyeLxkjeNnQh@dpg-d488aundiees739psf40-a.ohio-postgres.render.com/fair_db"
 
 
-DATABASE_URL = os.environ.get("DATABASE_URL", RENDER_DB_URL)
+DATABASE_URL = os.environ.get(
+    "postgresql://postgres:guarantee@localhost:5432/fair_db", RENDER_DB_URL
+)
 
 engine = create_engine(DATABASE_URL)
 
