ded1ee93ceee5325a2473bf52978318c4fb0a76f
SHAMMAH <shamzyjayy@gmail.com>
2025-11-09 17:04:28 +0000

    cool commit

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ from sqlalchemy.orm import sessionmaker
 # engine = create_engine(DATABASE_URL)
 
 # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
+Base = declarative_base()
 
 
 # def get_db():
