d4d705067783c96610bac9a7fca18c3d11a1441a
SHAMMAH <shamzyjayy@gmail.com>
2025-11-09 13:00:51 +0000

    Fixed foreign key datatype issue and updated Student model

--- database.py
+++ database.py
@@ -3,9 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Example PostgreSQL URL format:
-# postgresql://username:password@hostname:port/databasename
-DATABASE_URL = os.environ.get("DATABASE_URL")
+
+RENDER_DB_URL = "postgresql://fair_db_user:Qtqs01CtbuJlxU3jQ1b2LyeLxkjeNnQh@dpg-d488aundiees739psf40-a.ohio-postgres.render.com/fair_db"
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL", RENDER_DB_URL)
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
